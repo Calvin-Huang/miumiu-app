3bf47ccd1eac332c682e54eeeb9a0479e7800cd4
Calvin Huang <calvin.peak@capslock.tw>
2017-02-15 17:26:33 +0800

    Define states.

--- App/Views/WayBills.js
+++ App/Views/WayBills.js
@@ -20,6 +20,19 @@ import dismissKeyboard from 'dismissKeyboard';
 
 import WayBill from './WayBill';
 
+const WayBillState = {
+  CONFIRMING: 0,
+  SHIPPING: 1,
+  ARRIVED: 2,
+};
+
+const UrgentState = {
+  NORMAL: 0,
+  REQUESTED: 1,
+  APPROVED: 2,
+  DECLINED: 3,
+};
+
 export default class WayBills extends Component {
   static propTypes = {
     rootComponent: PropTypes.object.isRequired,

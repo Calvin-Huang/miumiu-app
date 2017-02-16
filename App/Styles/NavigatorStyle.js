214d339e82eb0da8066c59bf137b9ef084baf1dc
Calvin Huang <calvin.peak@capslock.tw>
2017-02-16 17:16:32 +0800

    Add shared navigator styles.

---
+++ App/Styles/NavigatorStyle.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Calvin Huang on 2/16/17.
+ */
+
+export default styles = {
+  itemButton: {
+    margin: 9,
+    flex: 0,
+    color: 'white',
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
+};
---
+++ App/Styles/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Calvin Huang on 2/16/17.
+ */
+
+export { default as NavigatorStyle } from './NavigatorStyle';

7a023f7bed33abfb9d4d7ea756d8b936385b81f2
Calvin Huang <calvin.peak@capslock.tw>
2017-02-16 18:30:39 +0800

    Fix navigator font size.

--- App/Styles/NavigatorStyle.js
+++ App/Styles/NavigatorStyle.js
@@ -6,9 +6,11 @@ export default styles = {
   itemButton: {
     margin: 9,
     flex: 0,
+    fontSize: 17,
     color: 'white',
   },
   title: {
+    fontSize: 17,
     color: 'white',
     fontWeight: 'bold',
   }

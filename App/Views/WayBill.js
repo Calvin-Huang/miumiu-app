73537ff85f07d23a0102e139447907e331500c49
Calvin Huang <calvin.peak@capslock.tw>
2017-02-16 18:31:08 +0800

    Complete basic WayBill layout.

--- App/Views/WayBill.js
+++ App/Views/WayBill.js
@@ -9,8 +9,11 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { NavigatorComponent, MiumiuThemeNavigatorBackground } from '../Components';
-import { NavigatorStyle } from '../Styles';
+import { NavigatorStyle, MiumiuTheme } from '../Styles';
+import { stateInfoMapping } from '../Constants/states';
 
 export default class WayBill extends NavigatorComponent {
   static navRightButton({ data: { id } }, navigator, index, navState) {
@@ -32,11 +35,38 @@ export default class WayBill extends NavigatorComponent {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <MiumiuThemeNavigatorBackground />
-        <View>
+    const { icon, iconColor, title } = stateInfoMapping[this.props.route.data.state] || {};
 
+    return (
+      <View style={MiumiuTheme.container}>
+        <MiumiuThemeNavigatorBackground />
+        <View style={styles.sectionHeader}>
+          <Icon style={styles.sectionIcon} name={icon} size={24} color={iconColor} />
+          <Text style={styles.sectionText}>待確認訂單，您可以申請加急服務</Text>
+        </View>
+        <View style={styles.infoFieldContainer}>
+          <Text style={styles.infoFieldNameText}>
+            到貨日
+          </Text>
+          <Text style={styles.infoFieldValueText}>
+            -
+          </Text>
+        </View>
+        <View style={styles.infoFieldContainer}>
+          <Text style={styles.infoFieldNameText}>
+            到期日
+          </Text>
+          <Text style={styles.infoFieldValueText}>
+            -
+          </Text>
+        </View>
+        <View style={styles.infoFieldContainer}>
+          <Text style={styles.infoFieldNameText}>
+            金額
+          </Text>
+          <Text style={{ ...styles.infoFieldValueText, color: '#F6A623' }}>
+            -
+          </Text>
         </View>
       </View>
     )
@@ -44,9 +74,36 @@ export default class WayBill extends NavigatorComponent {
 }
 
 const styles = {
-  container: {
-    flex: 1,
-    flexDirection: 'column',
+  sectionHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  sectionIcon: {
+    marginLeft: 18,
+    marginRight: 10,
+    marginTop: 13,
+    marginBottom: 9,
+  },
+  sectionText: {
+    fontSize: 14,
+    color: 'rgba(0, 0, 0, 0.54)',
+  },
+  infoFieldContainer: {
     backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 1,
+  },
+  infoFieldNameText: {
+    fontSize: 16,
+    marginVertical: 16,
+    marginLeft: 17,
+  },
+  infoFieldValueText: {
+    flex: 1,
+    fontSize: 16,
+    marginVertical: 16,
+    marginRight: 17,
+    textAlign: 'right',
   },
 };

1568db5c0ed3d9973c04d35ff0be1995440722c7
Calvin Huang <calvin.peak@capslock.tw>
2017-02-16 15:53:52 +0800

    Change index to components index.

---
+++ App/Components/index.js
@@ -0,0 +1,3 @@
+export { default as MiumiuThemeNavigatorBackground } from './MiumiuThemeNavigatorBackground';
+export { default as IconFasterShipping } from './IconFasterShipping';
+export { default as WayBillStateView } from './WayBillStateView';

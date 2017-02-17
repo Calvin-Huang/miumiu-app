d6c649e19ab06bf5ad2b0021d2f409e4d9eef2a5
Calvin Huang <calvin.peak@capslock.tw>
2017-02-18 01:39:27 +0800

    Add empty UrgentProcessing component.

---
+++ App/Views/UrgentProcessing.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Calvin Huang on 2/18/17.
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+} from 'react-native';
+
+export default class UrgentProcessing extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}

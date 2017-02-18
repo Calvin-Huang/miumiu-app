df64448d59bad4380942f6cfa34de0faef8ce97d
Calvin Huang <calvin.peak@capslock.tw>
2017-02-19 00:29:45 +0800

    If UrgentProcessing component created with waybill id it will not showing id field.

--- App/Views/UrgentProcessing.js
+++ App/Views/UrgentProcessing.js
@@ -58,17 +58,19 @@ export default class UrgentProcessing extends NavigatorComponent {
         <View style={MiumiuTheme.container}>
           <MiumiuThemeNavigatorBackground />
           <View style={styles.body}>
-            <View style={styles.textFieldGroup}>
-              <MKTextField
-                floatingLabelEnabled={true}
-                textInputStyle={{ height: 31 }}
-                underlineSize={1}
-                highlightColor="#D8D8D8"
-                placeholder="請輸入單號"
-                placeholderTextColor="#9E9E9E"
-                style={{ backgroundColor: 'white' }}
-              />
-            </View>
+            { !data &&
+              <View style={styles.textFieldGroup}>
+                <MKTextField
+                  floatingLabelEnabled={true}
+                  textInputStyle={{ height: 31 }}
+                  underlineSize={1}
+                  highlightColor="#D8D8D8"
+                  placeholder="請輸入單號"
+                  placeholderTextColor="#9E9E9E"
+                  style={{ backgroundColor: 'white' }}
+                />
+              </View>
+            }
             <View style={styles.textFieldGroup}>
               <MKTextField
                 floatingLabelEnabled={true}

85bf2e8fcc1d13dfee9ff6dce4648017e3e7c943
Calvin Huang <calvin.peak@capslock.tw>
2017-02-20 18:41:41 +0800

    Rename basic button style and separate from action button style.

--- App/Styles/MiumiuTheme.js
+++ App/Styles/MiumiuTheme.js
@@ -2,6 +2,20 @@
  * Created by Calvin Huang on 2/16/17.
  */
 
+const sharedStyle = {
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 2,
+    paddingVertical: 11,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+  },
+}
+
 export default style = {
   container: {
     flex: 1,
@@ -25,23 +39,27 @@ export default style = {
   bulletContent: {
     flex: 1,
   },
-  actionButtonPrimary: {
+  buttonDefault: {
+    backgroundColor: '#9B9B9B',
+  },
+  buttonPrimary: {
     backgroundColor: '#4E9ACF',
   },
-  actionButtonWarning: {
+  buttonWarning: {
     backgroundColor: '#F5C163',
   },
+  button: sharedStyle.button,
+  buttonText: sharedStyle.buttonText,
   actionButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    ...sharedStyle.button,
+    borderRadius: 0,
     paddingVertical: 14,
   },
   actionButtonIcon: {
     marginRight: 10,
   },
   actionButtonText: {
-    color: 'white',
+    ...sharedStyle.buttonText,
     fontSize: 16,
   },
   textShadow: {

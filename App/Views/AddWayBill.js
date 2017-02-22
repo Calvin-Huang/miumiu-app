d375ffafb49b9cd2bda6c33a97624960a28193e9
Calvin Huang <calvin.peak@capslock.tw>
2017-02-22 11:10:15 +0800

    Add empty AddWayBill component.

---
+++ App/Views/AddWayBill.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Calvin Huang on 2/22/17.
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default class AddWayBill extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}

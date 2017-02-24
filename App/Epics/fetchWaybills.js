237c8d68d3da652280ba285a4594cf7a17337466
Calvin Huang <calvin.peak@capslock.tw>
2017-02-24 22:13:33 +0800

    Fix memory over stack caused by observer.

--- App/Epics/fetchWaybills.js
+++ App/Epics/fetchWaybills.js
@@ -2,6 +2,8 @@
  * Created by Calvin Huang on 2/24/17.
  */
 
+import * as ActionTypes from '../Constants/actionTypes';
+
 export default function fetchWayBills(action$) {
-  return action$;
+  return action$,ofType(ActionTypes.REQUESTED_WAYBILLS);
 }

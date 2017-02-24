5737b9e971d6147d082f0b3ce71aa1e6a6fb0a4b
Calvin Huang <calvin.peak@capslock.tw>
2017-02-24 14:56:31 +0800

    Add required reducer.

---
+++ App/Reducers/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Calvin Huang on 2/24/17.
+ */
+
+import { combineReducers } from 'redux';
+
+import wayBills from './wayBills';
+
+export default combineReducers({
+  wayBills,
+});

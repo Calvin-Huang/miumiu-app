2c7e46c8e8403770ece85781867a941b5027bde8
Calvin Huang <calvin.peak@capslock.tw>
2017-02-24 10:34:33 +0800

    Define action constants.

---
+++ App/Constants/actionTypes.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Calvin Huang on 2/23/17.
+ */
+
+export const OPEN_SIDE_DRAWER = 'OPEN_SIDE_DRAWER';
+export const CLOSE_SIDE_DRAWER = 'CLOSE_SIDE_DRAWER';
+
+export const HIDE_NAVIGATION_BAR = 'HIDE_NAVIGATION_BAR';
+export const SHOW_NAVIGATION_BAR = 'SHOW_NAVIGATION_BAR';

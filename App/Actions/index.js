ea8ee8a10a9ab65015ba12502ad143c7b0188396
Calvin Huang <calvin.peak@capslock.tw>
2017-02-24 22:14:32 +0800

    Change action wrap way.

--- App/Actions/index.js
+++ App/Actions/index.js
@@ -2,5 +2,5 @@
  * Created by Calvin Huang on 2/23/17.
  */
 
-export { defalut as NavigationBarActions } from './NavigationBarActions';
-export { default as SideDrawerActions } from './SideDrawerActions';
+export * from './navigationBarActions';
+export * from './sideDrawerActions';

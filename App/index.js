3b16985d4060eb399a83e529652e6a28bb9c5e48
Calvin Huang <calvin.peak@capslock.tw>
2017-02-26 05:28:13 +0800

    Use singleton store.

--- App/index.js
+++ App/index.js
@@ -5,9 +5,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
-import configureStore from './configureStore';
-
-const store = configureStore();
+import store from './storeInstance';
 
 import Main from './Containers/Main';
 

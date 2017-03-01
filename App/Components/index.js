c999acf16ff33614fb5278fcc6ffaff605ea04f1
Calvin Huang <calvin.peak@capslock.tw>
2017-03-01 13:53:21 +0800

    Add customised Navigator to component index.

--- App/Components/index.js
+++ App/Components/index.js
@@ -1,4 +1,5 @@
 export { default as Menu } from './Menu';
+export { default as Navigator } from './Navigator';
 export { default as NavigatorComponent } from './NavigatorComponent';
 export { default as MiumiuThemeNavigatorBackground } from './MiumiuThemeNavigatorBackground';
 export { default as IconFasterShipping } from './IconFasterShipping';

1978443d11bb30c3459f6b4f20645bc5f0a0cb8c
Calvin Huang <calvin.peak@capslock.tw>
2017-03-09 19:05:25 +0800

    Add empty FAQDetail container component.

---
+++ App/Containers/FAQDetail.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Calvin Huang on 3/9/17.
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+import { NavigatorComponent } from '../Components';
+
+export default class FAQDetail extends NavigatorComponent {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}

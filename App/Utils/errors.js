e1adb97aa8792847bc5ff9d2681ec556ce5179b8
Calvin Huang <calvin.peak@capslock.tw>
2017-03-14 16:53:31 +0800

    Add ExpandableError and add JWTExpiredError.

---
+++ App/Utils/errors.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Calvin Huang on 3/14/17.
+ */
+
+class ExpendableError extends Error {
+  constructor(message = '') {
+    super(message);
+
+    // extending Error is weird and does not propagate `message`
+    Object.defineProperty(this, 'message', {
+      configurable: true,
+      enumerable : false,
+      value : message,
+      writable : true,
+    });
+
+    Object.defineProperty(this, 'name', {
+      configurable: true,
+      enumerable : false,
+      value : this.constructor.name,
+      writable : true,
+    });
+
+    if (Error.hasOwnProperty('captureStackTrace')) {
+      Error.captureStackTrace(this, this.constructor);
+      return;
+    }
+
+    Object.defineProperty(this, 'stack', {
+      configurable: true,
+      enumerable : false,
+      value : (new Error(message)).stack,
+      writable : true,
+    });
+  }
+}
+
+
+export class JWTExpiredError extends ExpendableError {
+  constructor(message, jwtToken) {
+    super(message);
+
+    this.jwtToken = jwtToken;
+  }
+}

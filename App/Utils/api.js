8df5aab35b5784e80bfe0a454f72098f26a6aea3
Calvin Huang <calvin.peak@capslock.tw>
2017-03-14 21:57:16 +0800

    Complete API util.

---
+++ App/Utils/api.js
@@ -0,0 +1,116 @@
+/**
+ * Created by Calvin Huang on 3/14/17.
+ */
+
+// Reference to pepperoni-app-kit
+// https://github.com/futurice/pepperoni-app-kit
+
+import HttpError from 'standard-http-error';
+
+import { TIMEOUT, BASE_URL, REFRESH_TOKEN_END_POINT } from '../Constants/config';
+import { getAuthenticationToken, setAuthenticationToken } from './authentication';
+import { JWTExpiredError } from './errors';
+
+export function url(path) {
+  return path.indexOf('/') === 0 ? `${BASE_URL}${path}` : `${BASE_URL}/${path}`;
+}
+
+async function request(method, path, body, suppressRedBox) {
+  const endPoint = url(path);
+  let token;
+
+  try {
+    token = await getAuthenticationToken();
+  } catch (error) {
+    if (error instanceof JWTExpiredError) {
+      const { jwtToken: expiredToken } = error;
+      const refreshTokenEndPoint = url(REFRESH_TOKEN_END_POINT);
+      const headers = getRequestHeaders(null, expiredToken);
+
+      const response = await timeout(fetch(refreshTokenEndPoint, { headers }), TIMEOUT);
+      const { body: { token: newToken } } = await handleResponse(response);
+
+      token = newToken;
+
+      // Store new token to local storage.
+      setAuthenticationToken(newToken);
+    }
+  }
+
+  const headers = getRequestHeaders(body, token);
+  const options = body
+    ? { method, headers, body: JSON.stringify(body) }
+    : { method, headers };
+
+  const response = await timeout(fetch(endPoint, options), TIMEOUT);
+
+  return handleResponse(response);
+}
+
+function getRequestHeaders(body, token) {
+  const headers = body
+    ? { Accept: 'application/json', 'Content-Type': 'application/json' }
+    : { Accept: 'application/json' };
+
+  if (token) {
+    return {
+      ...headers,
+      'Authorization': `Bearer ${token}`,
+    };
+  }
+
+  return headers;
+}
+
+// Throw HttpError when status code bigger then 400.
+async function handleResponse(response) {
+  const { status, headers } = response;
+  if (status >= 400) {
+    const message = await getErrorMessageSafely(response);
+
+    throw new HttpError(status, message);
+  }
+
+  const responseBody = await response.text();
+  return {
+    status,
+    headers,
+    body: JSON.parse(responseBody),
+  };
+}
+
+async function getErrorMessageSafely(response) {
+  try {
+    const message = await response.text();
+
+    if (!message) {
+      return '';
+    }
+
+    // In miumiu project, all error structure is: { error: [message] }
+    const { error } = JSON.parse(message);
+
+    return error || message;
+
+  } catch (error) {
+    return response._bodyInit;
+  }
+}
+
+function timeout(promise, ms) {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(() => {
+      reject(new Error('Request Timeout'));
+    }, ms);
+
+    promise
+      .then((response) => {
+        clearTimeout(timer);
+        resolve(response);
+      })
+      .reject((error) => {
+        clearTimeout(timer);
+        reject(error);
+      })
+  })
+}

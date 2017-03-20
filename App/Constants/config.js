d2069b886f1e8ed307eb2279000d6f964d852f42
Calvin Huang <calvin.peak@capslock.tw>
2017-03-20 15:07:03 +0800

    Rename REFRESH_TOKEN_END_POINT to REFRESH_TOKEN_PATH.

--- App/Constants/config.js
+++ App/Constants/config.js
@@ -5,4 +5,4 @@
 export const TIMEOUT = 6000;
 export const AUTHENTICATION_TOKEN_KEY = '';
 export const BASE_URL = '';
-export const REFRESH_TOKEN_END_POINT = `${BASE_URL}/token`;
+export const REFRESH_TOKEN_PATH = 'auth/token';

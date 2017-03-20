9137d6b1ea4062c71b0693890e1af96d6c09b0ea
Calvin Huang <calvin.peak@capslock.tw>
2017-03-20 11:41:08 +0800

    Rename fetchWayBills epic.

---
+++ App/Epics/fetchWayBills.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Calvin Huang on 2/24/17.
+ */
+
+import React from 'react-native';
+
+import * as ActionTypes from '../Constants/actionTypes';
+import { fetchWayBillsSuccess, fetchWayBillsFailed } from '../Actions/wayBillActions';
+import { get } from '../Utils/api';
+
+export default function fetchWayBills(action$) {
+  return action$,ofType(ActionTypes.REQUESTED_WAYBILLS)
+    .switchMap(async (action) => {
+      try {
+        const { data, per_page: perPage, current_page: currentPage, total } = await get('board', { page: action.page });
+        const hasNextPage = (perPage * currentPage) > total;
+
+        action.callback(data, { allLoaded: hasNextPage });
+
+        return fetchWayBillsSuccess(data);
+      } catch (error) {
+        return fetchWayBillsFailed(error);
+      }
+    });
+}
--- App/Epics/fetchWaybills.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Calvin Huang on 2/24/17.
- */
-
-import * as ActionTypes from '../Constants/actionTypes';
-
-export default function fetchWayBills(action$) {
-  return action$,ofType(ActionTypes.REQUESTED_WAYBILLS);
-}

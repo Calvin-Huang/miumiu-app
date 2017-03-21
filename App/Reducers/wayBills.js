92b6a05a4120e404311a3770f7f068f9420fb224
Calvin Huang <calvin.peak@capslock.tw>
2017-03-21 18:44:53 +0800

    - WayBill reducer will convert underscore rowData object key to camel case. - isFetching flag controls ActivityIndicator in footerView to shows user that will have next page, and is there will be next page, it should be true for infinity scroll.

--- App/Reducers/wayBills.js
+++ App/Reducers/wayBills.js
@@ -2,8 +2,56 @@
  * Created by Calvin Huang on 2/24/17.
  */
 
-const initialState = [];
+import {
+  REQUESTED_WAYBILLS,
+  RECEIVED_WAYBILLS_SUCCESS,
+  RECEIVED_WAYBILLS_FAILED,
+} from '../Constants/actionTypes';
+
+import humps from 'humps';
+
+import { WayBillState } from '../Constants/states';
+
+const initialState = {
+  data: [],
+  currentPage: 1,
+  isFetching: false,
+  error: null,
+};
 
 export default function wayBills(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case REQUESTED_WAYBILLS:
+      return {
+        ...state,
+        isFetching: true,
+        error: null,
+      };
+    case RECEIVED_WAYBILLS_SUCCESS:
+
+      const { data, per_page: perPage, current_page: currentPage, total } = action.response;
+      const hasNextPage = (perPage * currentPage) > total;
+
+      const filterCondition = (wayBill) => {
+
+        return wayBill.status !== WayBillState.PICKED_UP && wayBill.status !== WayBillState.UNKNOWN_OWNER
+      } ;
+
+      return {
+        data: [ ...state.data, ...humps.camelizeKeys(data.filter(filterCondition)) ],
+        currentPage,
+
+        // TRUE when need to fetch next page for infinity scroll.
+        isFetching: hasNextPage,
+        error: null,
+      };
+    case RECEIVED_WAYBILLS_FAILED:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
 }

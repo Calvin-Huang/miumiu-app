9434a3408d62bbcf06046b98bf48d5cebaceaed8
Calvin Huang <calvin.peak@capslock.tw>
2017-03-25 13:02:53 +0800

    Reduce FAQs response.

---
+++ App/Reducers/FAQs.js
@@ -0,0 +1,60 @@
+/**
+ * Created by calvin.huang on 24/03/2017.
+ */
+
+import {
+  FETCH_FAQS,
+  FETCH_FAQS_SUCCESS,
+  FETCH_FAQS_FAILED,
+  REFRESH_FAQS,
+  REFRESH_FAQS_SUCCESS,
+} from '../Constants/actionTypes';
+
+const initialState = {
+  isFetching: false,
+  isRefreshing: false,
+  data: [],
+  error: null,
+};
+
+export default function FAQs(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_FAQS:
+      return {
+        isFetching: true,
+        isRefreshing: false,
+        data: [],
+        error: null,
+      };
+    case FETCH_FAQS_SUCCESS:
+      return {
+        isFetching: false,
+        isRefreshing: false,
+        data: action.response,
+        error: null,
+      };
+    case FETCH_FAQS_FAILED:
+      return {
+        isFetching: false,
+        isRefreshing: false,
+        data: [],
+        error: action.error,
+      };
+    case REFRESH_FAQS:
+      return {
+        ...state,
+        isFetching: false,
+        isRefreshing: true,
+        error: null,
+      };
+    case REFRESH_FAQS_SUCCESS:
+      return {
+        isFetching: false,
+        isRefreshing: false,
+        data: action.response,
+        error: null,
+      };
+    default:
+      return state;
+  }
+}

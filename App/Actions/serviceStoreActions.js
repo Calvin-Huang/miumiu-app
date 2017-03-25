9c70219b4a21bb23d32e6abe78a4d7285b06fc9e
Calvin Huang <calvin.peak@capslock.tw>
2017-03-26 03:25:34 +0800

    Define service store actions and contsants.

---
+++ App/Actions/serviceStoreActions.js
@@ -0,0 +1,38 @@
+/**
+ * Created by calvin.huang on 26/03/2017.
+ */
+
+import * as Actions from '../Constants/actionTypes';
+
+export function fetchServiceStores() {
+  return {
+    type: Actions.FETCH_SERVICE_STORES,
+  };
+}
+
+export function fetchServiceStoresSuccess(response) {
+  return {
+    type: Actions.FETCH_SERVICE_STORES_SUCCESS,
+    response,
+  };
+}
+
+export function fetchServiceStoresFailed(error) {
+  return {
+    type: Actions.FETCH_SERVICE_STORES_SUCCESS,
+    error,
+  };
+}
+
+export function refreshServiceStores() {
+  return {
+    type: Actions.REFRESH_SERVICE_STORES,
+  };
+}
+
+export function refreshServiceStoresSuccess(response) {
+  return {
+    type: Actions.REFRESH_SERVICE_STORES_SUCCESS,
+    response,
+  };
+}

66f8621f4bed5b9bfbe5b371c3937adb92148a4a
Calvin Huang <calvin.peak@capslock.tw>
2017-03-26 12:06:33 +0800

    Reduce serviceStore and serviceStores data.

---
+++ App/Reducers/serviceStores.js
@@ -0,0 +1,73 @@
+/**
+ * Created by calvin.huang on 26/03/2017.
+ */
+
+import {
+  FETCH_SERVICE_STORES,
+  FETCH_SERVICE_STORES_SUCCESS,
+  FETCH_SERVICE_STORES_FAILED,
+  REFRESH_SERVICE_STORES,
+  REFRESH_SERVICE_STORES_SUCCESS,
+  FETCH_SERVICE_STORE,
+} from '../Constants/actionTypes';
+
+const initialState = {
+  isFetching: false,
+  isRefreshing: false,
+  data: [],
+  error: null,
+};
+
+export default function serviceStores(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_SERVICE_STORES:
+      return {
+        isFetching: true,
+        isRefreshing: false,
+        data: [],
+        error: null,
+      };
+    case FETCH_SERVICE_STORES_SUCCESS:
+      return {
+        isFetching: false,
+        isRefreshing: false,
+        data: action.response,
+        error: null,
+      };
+    case FETCH_SERVICE_STORES_FAILED:
+      return {
+        isFetching: false,
+        isRefreshing: false,
+        data: [],
+        error: action.error,
+      };
+    case REFRESH_SERVICE_STORES:
+      return {
+        ...state,
+        isFetching: false,
+        isRefreshing: true,
+        error: null,
+      };
+    case REFRESH_SERVICE_STORES_SUCCESS:
+      return {
+        isFetching: false,
+        isRefreshing: false,
+        data: action.response,
+        error: null,
+      };
+    case FETCH_SERVICE_STORE:
+      const { data } = state;
+      const { response } = action;
+
+      const serviceStore = data.find((eachData) => eachData.id === response.id);
+
+      data[data.indexOf(serviceStore)] = { ...serviceStore, ...response };
+
+      return {
+        ...state,
+        data,
+      };
+    default:
+      return state;
+  }
+}

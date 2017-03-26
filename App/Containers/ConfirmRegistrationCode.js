6feb2c3a70758fc97af1da338a123cdacfe86b8b
Calvin Huang <calvin.peak@capslock.tw>
2017-03-27 03:29:16 +0800

    Add transforming between phone and email type confirmation.

--- App/Containers/ConfirmRegistrationCode.js
+++ App/Containers/ConfirmRegistrationCode.js
@@ -15,10 +15,12 @@ import {
 import dismissKeyboard from 'dismissKeyboard';
 
 import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import { NavigatorComponent } from '../Components';
 import RegistrationCompleted from './RegistrationCompleted';
 import { MiumiuTheme } from '../Styles';
+import { validateEmail } from '../Utils/validator';
 
 export default class ConfirmRegistrationCode extends NavigatorComponent {
   constructor(props) {
@@ -26,6 +28,7 @@ export default class ConfirmRegistrationCode extends NavigatorComponent {
 
     this.state = {
       codes: [ '', '', '', '' ],
+      isAccountTypeEmail: validateEmail(props.route.data.account),
     };
   }
 
@@ -48,6 +51,7 @@ export default class ConfirmRegistrationCode extends NavigatorComponent {
   }
 
   render() {
+    const { isAccountTypeEmail } = this.state;
     return (
       <TouchableWithoutFeedback onPress={() => { dismissKeyboard(); }}>
         <LinearGradient
@@ -58,40 +62,47 @@ export default class ConfirmRegistrationCode extends NavigatorComponent {
         >
           <View style={styles.body}>
             <Text style={styles.registerHintText}>
-              請收取驗證碼，訊息可能延遲一兩分鐘送出
+              { isAccountTypeEmail ? '請收取驗證信件，信件可能延遲一兩分鐘送出' : '請收取驗證碼，訊息可能延遲一兩分鐘送出' }
             </Text>
-            <View style={styles.codeFieldGroup}>
-              <TextInput ref="codeField1" keyboardType="numeric" maxLength={1} style={styles.codeField} autoFocus={true} onChangeText={this.codeFieldTextChanged.bind(this, 1)} />
-              <TextInput ref="codeField2" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 2)} />
-              <TextInput ref="codeField3" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 3)} />
-              <TextInput ref="codeField4" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 4)} />
-            </View>
+            { isAccountTypeEmail &&
+              <Icon style={{ alignSelf: 'center' }} name="md-mail" color="white" size={40} />
+            }
+            { !isAccountTypeEmail &&
+              <View style={styles.codeFieldGroup}>
+                <TextInput ref="codeField1" keyboardType="numeric" maxLength={1} style={styles.codeField} autoFocus={true} onChangeText={this.codeFieldTextChanged.bind(this, 1)} />
+                <TextInput ref="codeField2" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 2)} />
+                <TextInput ref="codeField3" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 3)} />
+                <TextInput ref="codeField4" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 4)} />
+              </View>
+            }
             <Text style={styles.registerHintText}>
-              如果沒有收到驗證碼，您可以在 <Text>0:20</Text> 後
+              如果沒有收到驗證{ isAccountTypeEmail ? '信件' : '碼' }，您可以在 <Text>0:20</Text> 後
             </Text>
             <TouchableOpacity>
-              <Text style={{ ...MiumiuTheme.buttonText, ...styles.underline }}>重新發送驗證碼</Text>
+              <Text style={{ ...MiumiuTheme.buttonText, ...styles.underline }}>重新發送驗證{ isAccountTypeEmail ? '信件' : '碼' }</Text>
             </TouchableOpacity>
           </View>
-          <TouchableOpacity
-            style={{ ...MiumiuTheme.actionButton, ...MiumiuTheme.roundButton }}
-            onPress={() => {
-              dismissKeyboard();
+          { !isAccountTypeEmail &&
+            <TouchableOpacity
+              style={{ ...MiumiuTheme.actionButton, ...MiumiuTheme.roundButton }}
+              onPress={() => {
+                dismissKeyboard();
 
-              // Disable swipe back gesture.
-              this.pushToNextComponent(RegistrationCompleted, null, { ...Navigator.SceneConfigs.PushFromRight, gestures: {} });
-            }}
-          >
-            <LinearGradient
-              start={{ x: 0.485544682, y: 1.44908902 }} end={{ x: 0.485544682, y: -0.811377672 }}
-              locations={[0, 0.0770538389, 0.605226529, 1]}
-              colors={['#57C9EB', '#55BCE3', '#4E9ACF', '#487ABD']}
-              style={styles.roundButtonBackground}
-            />
-            <Text style={MiumiuTheme.buttonText}>
-              送出
-            </Text>
-          </TouchableOpacity>
+                // Disable swipe back gesture.
+                this.pushToNextComponent(RegistrationCompleted, null, { ...Navigator.SceneConfigs.PushFromRight, gestures: {} });
+              }}
+            >
+              <LinearGradient
+                start={{ x: 0.485544682, y: 1.44908902 }} end={{ x: 0.485544682, y: -0.811377672 }}
+                locations={[0, 0.0770538389, 0.605226529, 1]}
+                colors={['#57C9EB', '#55BCE3', '#4E9ACF', '#487ABD']}
+                style={styles.roundButtonBackground}
+              />
+              <Text style={MiumiuTheme.buttonText}>
+                送出
+              </Text>
+            </TouchableOpacity>
+          }
         </LinearGradient>
       </TouchableWithoutFeedback>
     );

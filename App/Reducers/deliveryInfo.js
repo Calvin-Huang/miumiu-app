5bdbc3e1e5b056f536f1e1b5e146a16351d514ec
Calvin Huang <calvin.peak@capslock.tw>
2017-03-26 21:22:40 +0800

    Add delivery info list and detail reducer.

---
+++ App/Reducers/deliveryInfo.js
@@ -0,0 +1,38 @@
+/**
+ * Created by calvin.huang on 26/03/2017.
+ */
+
+import { FETCH_DELIVERY_INFO, FETCH_DELIVERY_INFO_SUCCESS, FETCH_DELIVERY_INFO_FAILED } from '../Constants/actionTypes';
+
+const initialState = {
+  id: null,
+  isFetching: false,
+  error: null,
+};
+
+export default function deliveryInfo(state = initialState, action) {
+  switch (action.type) {
+
+    // Concat existed ServiceStore.
+    case FETCH_DELIVERY_INFO:
+      return {
+        id: action.id,
+        isFetching: true,
+        error: null,
+      };
+    case FETCH_DELIVERY_INFO_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: null,
+      };
+    case FETCH_DELIVERY_INFO_FAILED:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}

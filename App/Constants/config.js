736cac761defc26dd9c851d57b97e6729fb23d11
Calvin Huang <calvin.peak@capslock.tw>
2017-03-29 04:39:40 +0800

    Add domain setting to config.

--- App/Constants/config.js
+++ App/Constants/config.js
@@ -8,6 +8,7 @@ export const TIMEOUT = 6000;
 export const AUTHENTICATION_TOKEN_KEY = '';
 export const BASE_URL = '';
 export const COUNTDOWN_SECONDS = '100';
+export const DOMAIN = '';
 export const REFRESH_TOKEN_PATH = 'auth/token';
 export const DATETIME_FORMAT = 'YYYY/MM/DD HH:mm';
 export const DEEP_LINK_PROTOCOL = '';

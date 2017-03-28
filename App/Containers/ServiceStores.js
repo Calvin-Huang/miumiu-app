1be7b501b0e62b3fbd79bcfad7cd18960276a39f
Calvin Huang <calvin.peak@capslock.tw>
2017-03-29 02:50:47 +0800

    Add search filter in ServiceStores.

--- App/Containers/ServiceStores.js
+++ App/Containers/ServiceStores.js
@@ -130,10 +130,27 @@ class ServiceStores extends NavigatorComponent {
     ]).start();
 
     this.setState({ isSearching: false });
+    this.filterSearchResult(null);
   }
 
   searchBarTextChanged(text) {
+    this.filterSearchResult(text);
+  }
 
+  filterSearchResult(term) {
+    const { isSearching } = this.state;
+    const { stores } = this.props;
+    if (!term) {
+      this.setState({
+        stores: dataSource.cloneWithRows(stores),
+      });
+    } else if (isSearching) {
+      this.setState({
+        stores: dataSource.cloneWithRows(
+          stores.filter(({ name, address }) => (name || '').includes(term) || (address || '').includes(term))
+        ),
+      });
+    }
   }
 
   renderRowView(rowData, sectionID, rowID, highlightRow) {

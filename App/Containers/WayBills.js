2a6cd160ee0507a3072fe4196d37691824c769a7
Calvin Huang <calvin.peak@capslock.tw>
2017-03-29 02:44:15 +0800

    Add search filter.

--- App/Containers/WayBills.js
+++ App/Containers/WayBills.js
@@ -123,9 +123,14 @@ class WayBills extends NavigatorComponent {
   }
 
   componentWillReceiveProps(props) {
-    this.setState({
-      wayBills: dataSource.cloneWithRows(props.wayBills),
-    });
+    const { searchingTerm } = this;
+    if (searchingTerm) {
+      this.filterSearchResult(searchingTerm);
+    } else {
+      this.setState({
+        wayBills: dataSource.cloneWithRows(props.wayBills),
+      });
+    }
   }
 
   showSearchBar() {
@@ -195,10 +200,29 @@ class WayBills extends NavigatorComponent {
     ]).start();
 
     this.setState({ isSearching: false });
+
+    this.searchingTerm = null;
+    this.filterSearchResult(null);
   }
 
   searchBarTextChanged(text) {
+    this.searchingTerm = text;
 
+    this.filterSearchResult(this.searchingTerm);
+  }
+
+  filterSearchResult(term) {
+    const { isSearching } = this.state;
+    const { wayBills } = this.props;
+    if (!term) {
+      this.setState({
+        wayBills: dataSource.cloneWithRows(wayBills),
+      });
+    } else if (isSearching) {
+      this.setState({
+        wayBills: dataSource.cloneWithRows(wayBills.filter(({ shippingNo }) => shippingNo.includes(term))),
+      });
+    }
   }
   
   onPaginating() {

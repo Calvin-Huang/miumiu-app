701e6f036923c1caee296a0c1a920a14af42beb4
Calvin Huang <calvin.peak@capslock.tw>
2017-04-25 15:37:43 +0800

    Add check version actions.

---
+++ App/Actions/checkVersionActions.js
@@ -0,0 +1,18 @@
+/**
+ * Created by calvin.huang on 25/04/2017.
+ */
+
+import * as ActionTypes from '../Constants/actionTypes';
+
+export function fetchCurrentVersionInfo() {
+  return {
+    type: ActionTypes.FETCH_CURRENT_VERSION_INFO,
+  }
+}
+
+export function showVersionOutdatedHint(isForceUpdate) {
+  return {
+    type: ActionTypes.SHOW_VERSION_OUTDATED_HINT,
+    isForceUpdate,
+  }
+}

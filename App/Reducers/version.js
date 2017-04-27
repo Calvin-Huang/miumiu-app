e6c47012ead3f227fd9b283560822a2185b913a2
Calvin Huang <calvin.peak@capslock.tw>
2017-04-27 15:44:06 +0800

    Add version reducer.

--- App/Reducers/version.js
+++ App/Reducers/version.js
@@ -1,3 +1,29 @@
 /**
  * Created by calvin.huang on 27/04/2017.
  */
+
+import { SHOW_VERSION_OUTDATED_HINT, HIDE_VERSION_OUTDATED_HINT } from '../Constants/actionTypes';
+
+const initialState = {
+  show: false,
+  versionName: '',
+  forceUpdate: false,
+};
+
+export function needUpdateModal(state = initialState, action) {
+  switch (action.type) {
+    case SHOW_VERSION_OUTDATED_HINT:
+      return {
+        show: true,
+        versionName: action.versionName,
+        forceUpdate: action.isForceUpdate,
+      };
+    case HIDE_VERSION_OUTDATED_HINT:
+      return {
+        ...state,
+        show: false,
+      };
+    default:
+      return state;
+  }
+}

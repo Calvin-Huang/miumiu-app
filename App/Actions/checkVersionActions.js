ad7933637acd71000aa6a7198028a02d3ec034d3
Calvin Huang <calvin.peak@capslock.tw>
2017-04-27 13:07:53 +0800

    Fix code style.

--- App/Actions/checkVersionActions.js
+++ App/Actions/checkVersionActions.js
@@ -7,12 +7,12 @@ import * as ActionTypes from '../Constants/actionTypes';
 export function fetchCurrentVersionInfo() {
   return {
     type: ActionTypes.FETCH_CURRENT_VERSION_INFO,
-  }
+  };
 }
 
 export function showVersionOutdatedHint(isForceUpdate) {
   return {
     type: ActionTypes.SHOW_VERSION_OUTDATED_HINT,
     isForceUpdate,
-  }
+  };
 }

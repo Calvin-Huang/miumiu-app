770677312882bcae26392595cb6565a7fe0ac63a
Calvin Huang <calvin.peak@capslock.tw>
2017-05-08 15:20:08 +0800

    Remove used state.

--- App/Containers/FAQ.js
+++ App/Containers/FAQ.js
@@ -56,8 +56,6 @@ class FAQ extends NavigatorComponent {
     super(props);
 
     this.state = {
-      showNavButton: true,
-      showTitle: true,
       navBarStretchValue: new Animated.Value(104),
       searchBarMarginBottom: new Animated.Value(9),
       cancelButtonMarginRight: new Animated.Value(-45),

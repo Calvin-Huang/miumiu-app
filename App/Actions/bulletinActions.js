94a0d7a28b5bff2da3ba8062f093eb23225bbf4c
Calvin Huang <calvin.peak@capslock.tw>
2017-05-09 13:10:48 +0800

    Define bulletin actions.

---
+++ App/Actions/bulletinActions.js
@@ -0,0 +1,46 @@
+/**
+ * Created by calvin.huang on 08/05/2017.
+ */
+
+import * as actionTypes from '../Constants/actionTypes';
+
+export function fetchBulletinBoard(currentPage) {
+  return {
+    type: actionTypes.FETCH_BULLETIN_BOARD,
+    currentPage,
+  }
+}
+
+export function fetchBulletinBoardSuccess(response) {
+  return {
+    type: actionTypes.FETCH_BULLETIN_BOARD_SUCCESS,
+    response,
+  }
+}
+
+export function fetchBulletinBoardFailed(error, atPage) {
+  return {
+    type: actionTypes.FETCH_BULLETIN_BOARD_FAILED,
+    error,
+    atPage,
+  }
+}
+
+export function refreshBulletinBoard() {
+  return {
+    type: actionTypes.REFRESH_BULLETIN_BOARD,
+  }
+}
+
+export function refreshBulletinBoardSuccess(response) {
+  return {
+    type: actionTypes.REFRESH_BULLETIN_BOARD_SUCCESS,
+    response,
+  }
+}
+
+export function refreshBulletinBoardFailed(error) {
+  return {
+    error,
+  }
+}

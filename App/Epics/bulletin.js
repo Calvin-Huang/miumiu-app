7017055d44fa24ac269c14960ca21fe2674576aa
Calvin Huang <calvin.peak@capslock.tw>
2017-05-09 16:39:33 +0800

    Follow previous code style.

--- App/Epics/bulletin.js
+++ App/Epics/bulletin.js
@@ -2,11 +2,7 @@
  * Created by calvin.huang on 08/05/2017.
  */
 
-import {
-  FETCH_BULLETIN_BOARD,
-  REFRESH_BULLETIN_BOARD,
-  SEARCH_BULLETIN_BOARD,
-} from '../Constants/actionTypes';
+import * as ActionTypes from '../Constants/actionTypes';
 import {
   fetchBulletinBoard as fetchBulletinBoardAction,
   fetchBulletinBoardSuccess,
@@ -18,7 +14,7 @@ import {
 import { get } from '../Utils/api';
 
 export function fetchBulletinBoard(action$, store) {
-  return action$.ofType(FETCH_BULLETIN_BOARD)
+  return action$.ofType(ActionTypes.FETCH_BULLETIN_BOARD)
     .exhaustMap(async (action) => {
       try {
         const response = await get(`board?page=${action.currentPage}&q=${store.getState().bulletinBoard.query}`);
@@ -31,7 +27,7 @@ export function fetchBulletinBoard(action$, store) {
 }
 
 export function searchBulletinBoard(action$) {
-  return action$.ofType(SEARCH_BULLETIN_BOARD)
+  return action$.ofType(ActionTypes.SEARCH_BULLETIN_BOARD)
     .throttleTime(200)
     .map((_) => {
       return fetchBulletinBoardAction(1);
@@ -39,7 +35,7 @@ export function searchBulletinBoard(action$) {
 }
 
 export function refreshBulletinBoard(action$, store) {
-  return action$.ofType(REFRESH_BULLETIN_BOARD)
+  return action$.ofType(ActionTypes.REFRESH_BULLETIN_BOARD)
     .switchMap(async (_) => {
       try {
         const response = await get(`board?page=1&q=${store.getState().bulletinBoard.query}`);

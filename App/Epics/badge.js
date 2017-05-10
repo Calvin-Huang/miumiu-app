58cd5678969b35c7e8b32e678ef4ce491b92b2c4
Calvin Huang <calvin.peak@capslock.tw>
2017-05-10 14:31:51 +0800

    Handle promise rejected situation.

--- App/Epics/badge.js
+++ App/Epics/badge.js
@@ -2,6 +2,8 @@
  * Created by calvin.huang on 10/05/2017.
  */
 
+import { Observable } from 'rxjs';
+
 import * as ActionTypes from '../Constants/actionTypes';
 import { updateBadges } from '../Actions/badgeActions';
 
@@ -9,18 +11,32 @@ import { get, del } from '../Utils/api';
 
 export function fetchBadges(action$) {
   return action$.ofType(ActionTypes.FETCH_BADGES)
-    .switchMap(async (_) => {
-      const response = await get('badges');
+    .switchMap((_) => {
+      return new Observable(async (observer) => {
+        try {
+          const response = await get('badges');
 
-      return updateBadges(response);
+          observer.next(updateBadges(response));
+        } catch (error) {
+          // Do nothing when API failed.
+        }
+        observer.complete();
+      });
     });
 }
 
 export function removeBadge(action$) {
   return action$.ofType(ActionTypes.REMOVE_BADGE)
-    .switchMap(async (action) => {
-      const response = await del(`badges/${action.badge}`);
+    .switchMap((action) => {
+      return new Observable(async (observer) => {
+        try {
+          const response = await del(`badges/${action.badge}`);
 
-      return updateBadges(response);
+          observer.next(updateBadges(response));
+        } catch (error) {
+          // Do nothing when API failed.
+        }
+        observer.complete();
+      });
     });
 }

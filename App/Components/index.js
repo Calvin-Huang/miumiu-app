57277e956d679dfe9582088ecc0fea49a7dc9edc
Calvin Huang <calvin.peak@capslock.tw>
2017-05-15 19:30:51 +0800

    Add NotificationMessage to component index.

--- App/Components/index.js
+++ App/Components/index.js
@@ -1,3 +1,4 @@
+export { default as NotificationMessage } from './NotificationMessage';
 export { default as Menu } from './Menu';
 export { default as Navigator } from './Navigator';
 export { default as NavigatorComponent } from './NavigatorComponent';

e4916051636274afdeb691ad78e803d5b3a1d534
Calvin Huang <calvin.peak@capslock.tw>
2017-05-31 06:04:32 -0700

    Fix eslint warning in ForgetPassword container.

--- App/Containers/ForgetPassword.js
+++ App/Containers/ForgetPassword.js
@@ -2,14 +2,13 @@
  * Created by calvin.huang on 27/03/2017.
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   View,
   Text,
   TouchableOpacity,
   TouchableWithoutFeedback,
   ActivityIndicator,
-  Alert,
 } from 'react-native';
 
 import dismissKeyboard from 'dismissKeyboard';
@@ -22,96 +21,6 @@ import { NavigatorComponent } from '../Components';
 import ConfirmResetPasswordCode from './ConfirmResetPasswordCode';
 import { MiumiuTheme } from '../Styles';
 import { generalRequestFailed, userRequestResetPassword } from '../Actions';
-
-class ForgetPassword extends NavigatorComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      account: null,
-    }
-  }
-
-  componentWillReceiveProps(props) {
-    const { navigator, route } = props;
-    const currentRoute = navigator.getCurrentRoutes()[navigator.getCurrentRoutes().length - 1];
-    if (currentRoute.index === route.index && !props.isRequesting && !props.error && props.timestamp) {
-      const { timestamp } = this.props;
-      const { account } = this.state;
-
-      this.pushToNextComponent(ConfirmResetPasswordCode, { account, timestamp });
-    }
-  }
-
-  submitRequest() {
-    if (this.props.isRequesting) {
-      return;
-    }
-
-    const { account } = this.state;
-    if (account) {
-      this.props.userRequestResetPassword(account);
-    }
-  }
-
-  render() {
-    const { account } = this.state;
-    const { isRequesting, error } = this.props;
-
-    return (
-      <TouchableWithoutFeedback onPress={() => { dismissKeyboard(); }}>
-        <LinearGradient
-          start={{ x: 0.510023057, y: 1.09617584 }} end={{ x: 0.51374295, y: -0.0557819706 }}
-          locations={[0, 0.23, 0.66, 1]}
-          colors={['#57C9EB', '#55BCE3', '#4E9ACF', '#487ABD']}
-          style={styles.container}
-        >
-          <View style={styles.body}>
-            <View style={MiumiuTheme.textFieldGroup}>
-              <MKTextField
-                autoCapitalize="none"
-                keyboardType="email-address"
-                floatingLabelEnabled={true}
-                textInputStyle={{ height: 31 }}
-                underlineSize={1}
-                highlightColor="#D8D8D8"
-                placeholder="手機號碼"
-                placeholderTextColor="#9E9E9E"
-                style={{ backgroundColor: 'white' }}
-                onChangeText={(account) => { this.setState({ account }); }}
-                value={account}
-              />
-            </View>
-            { error &&
-              <View style={MiumiuTheme.textFieldGroup}>
-                <Text style={MiumiuTheme.errorText}>
-                  {error.message}
-                </Text>
-              </View>
-            }
-          </View>
-          <TouchableOpacity
-            style={{ ...MiumiuTheme.actionButton, ...MiumiuTheme.roundButton }}
-            onPress={this.submitRequest.bind(this)}
-          >
-            <LinearGradient
-              start={{ x: 0.485544682, y: 1.44908902 }} end={{ x: 0.485544682, y: -0.811377672 }}
-              locations={[0, 0.0770538389, 0.605226529, 1]}
-              colors={['#57C9EB', '#55BCE3', '#4E9ACF', '#487ABD']}
-              style={styles.roundButtonBackground}
-            />
-            <Text style={MiumiuTheme.buttonText}>
-              重設密碼
-            </Text>
-            { isRequesting &&
-              <ActivityIndicator color="white" style={MiumiuTheme.buttonActivityIndicator} />
-            }
-          </TouchableOpacity>
-        </LinearGradient>
-      </TouchableWithoutFeedback>
-    );
-  }
-}
 
 const styles = {
   container: {
@@ -155,6 +64,98 @@ const styles = {
     bottom: 0,
     left: 0,
   },
+};
+
+class ForgetPassword extends NavigatorComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      account: null,
+    };
+
+    this.submitRequest = this.submitRequest.bind(this);
+  }
+
+  componentWillReceiveProps(props) {
+    const { navigator, route } = props;
+    const currentRoute = navigator.getCurrentRoutes()[navigator.getCurrentRoutes().length - 1];
+    if (currentRoute.index === route.index && !props.isRequesting && !props.error && props.timestamp) {
+      const { timestamp } = this.props;
+      const { account } = this.state;
+
+      this.pushToNextComponent(ConfirmResetPasswordCode, { account, timestamp });
+    }
+  }
+
+  submitRequest() {
+    if (this.props.isRequesting) {
+      return;
+    }
+
+    const { account } = this.state;
+    if (account) {
+      this.props.userRequestResetPassword(account);
+    }
+  }
+
+  render() {
+    const { account } = this.state;
+    const { isRequesting, error } = this.props;
+
+    return (
+      <TouchableWithoutFeedback onPress={() => { dismissKeyboard(); }}>
+        <LinearGradient
+          start={{ x: 0.510023057, y: 1.09617584 }} end={{ x: 0.51374295, y: -0.0557819706 }}
+          locations={[0, 0.23, 0.66, 1]}
+          colors={['#57C9EB', '#55BCE3', '#4E9ACF', '#487ABD']}
+          style={styles.container}
+        >
+          <View style={styles.body}>
+            <View style={MiumiuTheme.textFieldGroup}>
+              <MKTextField
+                autoCapitalize="none"
+                keyboardType="email-address"
+                floatingLabelEnabled
+                textInputStyle={{ height: 31 }}
+                underlineSize={1}
+                highlightColor="#D8D8D8"
+                placeholder="手機號碼"
+                placeholderTextColor="#9E9E9E"
+                style={{ backgroundColor: 'white' }}
+                onChangeText={(text) => { this.setState({ account: text }); }}
+                value={account}
+              />
+            </View>
+            { error &&
+              <View style={MiumiuTheme.textFieldGroup}>
+                <Text style={MiumiuTheme.errorText}>
+                  {error.message}
+                </Text>
+              </View>
+            }
+          </View>
+          <TouchableOpacity
+            style={{ ...MiumiuTheme.actionButton, ...MiumiuTheme.roundButton }}
+            onPress={this.submitRequest}
+          >
+            <LinearGradient
+              start={{ x: 0.485544682, y: 1.44908902 }} end={{ x: 0.485544682, y: -0.811377672 }}
+              locations={[0, 0.0770538389, 0.605226529, 1]}
+              colors={['#57C9EB', '#55BCE3', '#4E9ACF', '#487ABD']}
+              style={styles.roundButtonBackground}
+            />
+            <Text style={MiumiuTheme.buttonText}>
+              重設密碼
+            </Text>
+            { isRequesting &&
+              <ActivityIndicator color="white" style={MiumiuTheme.buttonActivityIndicator} />
+            }
+          </TouchableOpacity>
+        </LinearGradient>
+      </TouchableWithoutFeedback>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -165,9 +166,9 @@ const mapStateToProps = (state, ownProps) => {
     isRequesting: generalRequest.isRequesting,
     error: generalRequest.error,
   };
-}
+};
 
 export default connect(
   mapStateToProps,
-  { generalRequestFailed, userRequestResetPassword }
+  { generalRequestFailed, userRequestResetPassword },
 )(ForgetPassword);

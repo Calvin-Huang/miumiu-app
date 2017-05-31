e51427d8f7517d5b27709bc992bccc01eb3c6010
Calvin Huang <calvin.peak@capslock.tw>
2017-05-31 06:56:38 -0700

    Fix PickUpPassword eslint warning.

--- App/Containers/PickUpPassword.js
+++ App/Containers/PickUpPassword.js
@@ -2,7 +2,7 @@
  * Created by Calvin Huang on 3/11/17.
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   View,
   Text,
@@ -10,6 +10,7 @@ import {
   KeyboardAvoidingView,
   TouchableOpacity,
   TouchableWithoutFeedback,
+  Platform,
 } from 'react-native';
 
 import { connect } from 'react-redux';
@@ -23,104 +24,6 @@ import { NavigatorComponent, MiumiuThemeNavigatorBackground } from '../Component
 import { MiumiuTheme, NavigatorStyle } from '../Styles';
 import { openSideDrawer } from '../Actions';
 import store from '../storeInstance';
-
-class PickUpPassword extends NavigatorComponent {
-  static navLeftButton(route, navigator, index, navState) {
-    return (
-      <TouchableOpacity
-        onPress={() => { store.dispatch(openSideDrawer()); }}
-      >
-        <View style={NavigatorStyle.itemButton}>
-          <Icon name="md-menu" size={24} color="white" />
-        </View>
-      </TouchableOpacity>
-    );
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      password: '',
-      codes: [ '', '', '', '' ],
-    };
-  }
-
-  codeFieldTextChanged(codeFieldNumber, text) {
-    const nextCodeField = this.refs[`codeField${codeFieldNumber + 1}`];
-
-    const codes = this.state.codes;
-    codes[codeFieldNumber] = text;
-    this.setState({
-      codes
-    });
-
-    if (text !== '') {
-      if (nextCodeField) {
-        nextCodeField.focus();
-      } else {
-        dismissKeyboard();
-      }
-    }
-  }
-
-  render() {
-    return (
-      <TouchableWithoutFeedback onPress={() => { dismissKeyboard(); }}>
-        <View style={MiumiuTheme.container}>
-          <MiumiuThemeNavigatorBackground>
-            <View style={NavigatorStyle.titleView}>
-              <Text style={NavigatorStyle.titleText}>
-                取貨鎖設定
-              </Text>
-            </View>
-          </MiumiuThemeNavigatorBackground>
-          <View style={styles.body}>
-            <View style={styles.pickUpPasswordSection}>
-              <Text style={MiumiuTheme.sectionText}>
-                請輸入取貨鎖密碼
-              </Text>
-              <View style={styles.codeFieldGroup}>
-                <TextInput ref="codeField1" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 1)} />
-                <TextInput ref="codeField2" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 2)} />
-                <TextInput ref="codeField3" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 3)} />
-                <TextInput ref="codeField4" keyboardType="numeric" maxLength={1} style={styles.codeField} onChangeText={this.codeFieldTextChanged.bind(this, 4)} />
-              </View>
-            </View>
-            <Text style={MiumiuTheme.sectionText}>
-              * 基於安全考量，請你協助我們確認是本人在操作
-            </Text>
-            <View style={MiumiuTheme.textFieldGroup}>
-              <MKTextField
-                floatingLabelEnabled={true}
-                textInputStyle={{ height: 31 }}
-                underlineSize={1}
-                highlightColor="#D8D8D8"
-                placeholder="請輸入密碼"
-                placeholderTextColor="#9E9E9E"
-                style={{ backgroundColor: 'white' }}
-                onChangeText={(password) => { this.setState({ password }); }}
-                value={this.state.password}
-              />
-            </View>
-          </View>
-          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : null}>
-            <View style={{ backgroundColor: Color(MiumiuTheme.buttonPrimary.backgroundColor).lighten(0.2), }}>
-              <TouchableOpacity
-                style={{ ...MiumiuTheme.actionButton, ...MiumiuTheme.buttonPrimary }}
-                onPress={() => {
-                dismissKeyboard();
-              }}
-              >
-                <Text style={MiumiuTheme.actionButtonText}>確認</Text>
-              </TouchableOpacity>
-            </View>
-          </KeyboardAvoidingView>
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  };
-}
 
 const styles = {
   body: {
@@ -160,11 +63,131 @@ const styles = {
   },
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return ownProps;
+class PickUpPassword extends NavigatorComponent {
+  static navLeftButton() {
+    return (
+      <TouchableOpacity
+        onPress={() => { store.dispatch(openSideDrawer()); }}
+      >
+        <View style={NavigatorStyle.itemButton}>
+          <Icon name="md-menu" size={24} color="white" />
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: '',
+      codes: ['', '', '', ''],
+    };
+  }
+
+  codeFieldTextChanged(codeFieldNumber, text) {
+    const nextCodeField = this.refs[`codeField${codeFieldNumber + 1}`];
+
+    const codes = this.state.codes;
+    codes[codeFieldNumber] = text;
+    this.setState({
+      codes,
+    });
+
+    if (text !== '') {
+      if (nextCodeField) {
+        nextCodeField.focus();
+      } else {
+        dismissKeyboard();
+      }
+    }
+  }
+
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={() => { dismissKeyboard(); }}>
+        <View style={MiumiuTheme.container}>
+          <MiumiuThemeNavigatorBackground>
+            <View style={NavigatorStyle.titleView}>
+              <Text style={NavigatorStyle.titleText}>
+                取貨鎖設定
+              </Text>
+            </View>
+          </MiumiuThemeNavigatorBackground>
+          <View style={styles.body}>
+            <View style={styles.pickUpPasswordSection}>
+              <Text style={MiumiuTheme.sectionText}>
+                請輸入取貨鎖密碼
+              </Text>
+              <View style={styles.codeFieldGroup}>
+                <TextInput
+                  ref={(ref) => { this.codeField1 = ref; }}
+                  keyboardType="numeric"
+                  maxLength={1}
+                  style={styles.codeField}
+                  onChangeText={text => this.codeFieldTextChanged(1, text)}
+                />
+                <TextInput
+                  ref={(ref) => { this.codeField2 = ref; }}
+                  keyboardType="numeric"
+                  maxLength={1}
+                  style={styles.codeField}
+                  onChangeText={text => this.codeFieldTextChanged(2, text)}
+                />
+                <TextInput
+                  ref={(ref) => { this.codeField3 = ref; }}
+                  keyboardType="numeric"
+                  maxLength={1}
+                  style={styles.codeField}
+                  onChangeText={text => this.codeFieldTextChanged(3, text)}
+                />
+                <TextInput
+                  ref={(ref) => { this.codeField4 = ref; }}
+                  keyboardType="numeric"
+                  maxLength={1}
+                  style={styles.codeField}
+                  onChangeText={text => this.codeFieldTextChanged(4, text)}
+                />
+              </View>
+            </View>
+            <Text style={MiumiuTheme.sectionText}>
+              * 基於安全考量，請你協助我們確認是本人在操作
+            </Text>
+            <View style={MiumiuTheme.textFieldGroup}>
+              <MKTextField
+                floatingLabelEnabled
+                textInputStyle={{ height: 31 }}
+                underlineSize={1}
+                highlightColor="#D8D8D8"
+                placeholder="請輸入密碼"
+                placeholderTextColor="#9E9E9E"
+                style={{ backgroundColor: 'white' }}
+                onChangeText={(password) => { this.setState({ password }); }}
+                value={this.state.password}
+              />
+            </View>
+          </View>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : null}>
+            <View style={{ backgroundColor: Color(MiumiuTheme.buttonPrimary.backgroundColor).lighten(0.2) }}>
+              <TouchableOpacity
+                style={{ ...MiumiuTheme.actionButton, ...MiumiuTheme.buttonPrimary }}
+                onPress={() => {
+                  dismissKeyboard();
+                }}
+              >
+                <Text style={MiumiuTheme.actionButtonText}>確認</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
 }
+
+const mapStateToProps = (state, ownProps) => ownProps;
 
 export default connect(
   mapStateToProps,
-  {}
+  {},
 )(PickUpPassword);

0d35a71ecee0b72ac1055c0080d2906b197d662f
Calvin Huang <calvin.peak@capslock.tw>
2017-05-31 03:06:49 -0700

    Fix eslint warning in ContactUs container.

--- App/Containers/ContactUs.js
+++ App/Containers/ContactUs.js
@@ -2,7 +2,7 @@
  * Created by calvin.huang on 24/03/2017.
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   View,
   ScrollView,
@@ -15,7 +15,7 @@ import {
 } from 'react-native';
 
 import { connect } from 'react-redux';
-
+import dismissKeyboard from 'dismissKeyboard';
 import Icon from 'react-native-vector-icons/Ionicons';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
@@ -25,163 +25,6 @@ import { openSideDrawer } from '../Actions/sideDrawerActions';
 import { fetchContactInfo } from '../Actions/settingActions';
 import store from '../storeInstance';
 import { currentUser } from '../Utils/authentication';
-
-class ContactUs extends NavigatorComponent {
-  static navLeftButton(route, navigator, index, navState) {
-    if (route.index === 0) {
-      return (
-        <TouchableOpacity onPress={() => { store.dispatch(openSideDrawer()); }}>
-          <View style={NavigatorStyle.itemButton}>
-            <Icon name="md-menu" size={24} color="white" />
-          </View>
-        </TouchableOpacity>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  static navRightButton(route, navigator, index, navState) {
-    if (route.index !== 0) {
-      return (
-        <TouchableOpacity onPress={() => {
-        dismissKeyboard();
-        navigator.pop();
-      }}>
-          <Text style={NavigatorStyle.itemTextButton}>
-            取消
-          </Text>
-        </TouchableOpacity>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  componentWillMount() {
-    if (!this.props.contactInfo || this.props.error) {
-      this.props.fetchContactInfo();
-    }
-  }
-
-  wechatInfoTapped(wechat) {
-    if (this.props.isRequesting) {
-      return;
-    }
-
-    const copyString = wechat;
-
-    Clipboard.setString(copyString);
-
-    this.refs.HUD.flash(2);
-
-    // When user doesn't install wechat, copy string to clipboard.
-    // console.log(`weixin://dl/chat?${wechat}`);
-    // Linking
-    //   .openURL(`weixin://dl/chat?${wechat}`)
-    //   .catch((_) => {
-    //     if (copyString) {
-    //       Clipboard.setString(copyString);
-    //
-    //       this.refs.HUD.flash(2);
-    //     }
-    //   });
-  }
-
-  phoneInfoTapped(mobile) {
-    if (this.props.isRequesting) {
-      return;
-    }
-
-    // Do nothing when user cancel phone call.
-    Linking
-      .openURL(`${Platform.OS !== 'android' ? 'telprompt' : 'tel'}:${mobile}`)
-      .catch(() => {});
-  }
-
-  async emailInfoTapped(email) {
-    if (this.props.isRequesting) {
-      return;
-    }
-
-    const user = await currentUser();
-
-    // Do nothing when user cancel sending email.
-    // [喵喵代收] 會員編號: ${user.id}
-    Linking
-      .openURL(`mailto:${email}?subject=%5B%E5%96%B5%E5%96%B5%E4%BB%A3%E6%94%B6%5D%20%E6%9C%83%E5%93%A1%E7%B7%A8%E8%99%9F%3A%20${user.id}`)
-      .catch((error) => {});
-  }
-
-  render() {
-    const { isRequesting, error, contactInfo } = this.props;
-    const { mobile, email, wechat } = contactInfo || {};
-    return (
-      <View style={MiumiuTheme.container}>
-        <MiumiuThemeNavigatorBackground>
-          <View style={NavigatorStyle.titleView}>
-            <Text style={NavigatorStyle.titleText}>
-              聯絡我們
-            </Text>
-          </View>
-        </MiumiuThemeNavigatorBackground>
-        <View style={styles.body}>
-          { error &&
-            <TouchableOpacity
-              style={{ ...MiumiuTheme.button, ...MiumiuTheme.buttonPrimary, margin: 10 }}
-              onPress={() => { this.props.fetchContactInfo(); }}
-            >
-              <Text style={MiumiuTheme.buttonText}>↻ 讀取失敗，重試一次</Text>
-            </TouchableOpacity>
-          }
-          { !error &&
-            <View>
-              <TouchableOpacity style={styles.row} onPress={this.wechatInfoTapped.bind(this, wechat)}>
-                <View style={styles.iconContainer}>
-                  <FontAwesomeIcon name="weixin" size={24} color="gray" />
-                </View>
-                <Text style={MiumiuTheme.listViewText}>
-                  {wechat}
-                </Text>
-                <Icon style={MiumiuTheme.listViewForwardIndicator} name="md-information-circle" size={22} color="#4285F4" />
-              </TouchableOpacity>
-              <View style={styles.separatorContainer}>
-                <View style={styles.separator} />
-              </View>
-              <TouchableOpacity style={styles.row} onPress={this.phoneInfoTapped.bind(this, mobile)}>
-                <View style={styles.iconContainer}>
-                  <Icon name="md-call" size={24} color="gray" />
-                </View>
-                <Text style={MiumiuTheme.listViewText}>
-                  {mobile}
-                </Text>
-                <Icon style={MiumiuTheme.listViewForwardIndicator} name="md-information-circle" size={22} color="#4285F4" />
-              </TouchableOpacity>
-              <View style={styles.separatorContainer}>
-                <View style={styles.separator} />
-              </View>
-              <TouchableOpacity style={styles.row} onPress={this.emailInfoTapped.bind(this, email)}>
-                <View style={styles.iconContainer}>
-                  <Icon name="md-mail" size={24} color="gray" />
-                </View>
-                <Text style={MiumiuTheme.listViewText}>
-                  {email}
-                </Text>
-                <Icon style={MiumiuTheme.listViewForwardIndicator} name="md-information-circle" size={22} color="#4285F4" />
-              </TouchableOpacity>
-            </View>
-          }
-          { isRequesting &&
-            <View style={MiumiuTheme.paginationView}>
-              <ActivityIndicator />
-            </View>
-          }
-        </View>
-        <HUD ref="HUD" type="success" message="已複製到剪貼簿" />
-      </View>
-    );
-  }
-}
 
 const styles = {
   body: {
@@ -216,6 +59,177 @@ const styles = {
   },
 };
 
+class ContactUs extends NavigatorComponent {
+  static navLeftButton(route) {
+    if (route.index === 0) {
+      return (
+        <TouchableOpacity onPress={() => { store.dispatch(openSideDrawer()); }}>
+          <View style={NavigatorStyle.itemButton}>
+            <Icon name="md-menu" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+      );
+    }
+    return null;
+  }
+
+  static navRightButton(route, navigator) {
+    if (route.index !== 0) {
+      return (
+        <TouchableOpacity
+          onPress={() => {
+            dismissKeyboard();
+            navigator.pop();
+          }}
+        >
+          <Text style={NavigatorStyle.itemTextButton}>
+            取消
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+    return null;
+  }
+
+  componentWillMount() {
+    if (!this.props.contactInfo || this.props.error) {
+      this.props.fetchContactInfo();
+    }
+  }
+
+  wechatInfoTapped(wechat) {
+    if (this.props.isRequesting) {
+      return;
+    }
+
+    const copyString = wechat;
+
+    Clipboard.setString(copyString);
+
+    this.HUD.flash(2);
+
+    // When user doesn't install wechat, copy string to clipboard.
+    // console.log(`weixin://dl/chat?${wechat}`);
+    // Linking
+    //   .openURL(`weixin://dl/chat?${wechat}`)
+    //   .catch((_) => {
+    //     if (copyString) {
+    //       Clipboard.setString(copyString);
+    //
+    //       this.HUD.flash(2);
+    //     }
+    //   });
+  }
+
+  phoneInfoTapped(mobile) {
+    if (this.props.isRequesting) {
+      return;
+    }
+
+    // Do nothing when user cancel phone call.
+    Linking
+      .openURL(`${Platform.OS !== 'android' ? 'telprompt' : 'tel'}:${mobile}`)
+      .catch(() => {});
+  }
+
+  async emailInfoTapped(email) {
+    if (this.props.isRequesting) {
+      return;
+    }
+
+    const user = await currentUser();
+
+    // Do nothing when user cancel sending email.
+    // [喵喵代收] 會員編號: ${user.id}
+    Linking
+      .openURL(`mailto:${email}?subject=%5B%E5%96%B5%E5%96%B5%E4%BB%A3%E6%94%B6%5D%20%E6%9C%83%E5%93%A1%E7%B7%A8%E8%99%9F%3A%20${user.id}`)
+      .catch(() => {});
+  }
+
+  render() {
+    const { isRequesting, error, contactInfo } = this.props;
+    const { mobile, email, wechat } = contactInfo || {};
+    return (
+      <View style={MiumiuTheme.container}>
+        <MiumiuThemeNavigatorBackground>
+          <View style={NavigatorStyle.titleView}>
+            <Text style={NavigatorStyle.titleText}>
+              聯絡我們
+            </Text>
+          </View>
+        </MiumiuThemeNavigatorBackground>
+        <View style={styles.body}>
+          { error &&
+            <TouchableOpacity
+              style={{ ...MiumiuTheme.button, ...MiumiuTheme.buttonPrimary, margin: 10 }}
+              onPress={() => { this.props.fetchContactInfo(); }}
+            >
+              <Text style={MiumiuTheme.buttonText}>↻ 讀取失敗，重試一次</Text>
+            </TouchableOpacity>
+          }
+          { !error &&
+            <View>
+              <TouchableOpacity style={styles.row} onPress={() => this.wechatInfoTapped(wechat)}>
+                <View style={styles.iconContainer}>
+                  <FontAwesomeIcon name="weixin" size={24} color="gray" />
+                </View>
+                <Text style={MiumiuTheme.listViewText}>
+                  {wechat}
+                </Text>
+                <Icon
+                  style={MiumiuTheme.listViewForwardIndicator}
+                  name="md-information-circle"
+                  size={22}
+                  color="#4285F4"
+                />
+              </TouchableOpacity>
+              <View style={styles.separatorContainer}>
+                <View style={styles.separator} />
+              </View>
+              <TouchableOpacity style={styles.row} onPress={() => this.phoneInfoTapped(mobile)}>
+                <View style={styles.iconContainer}>
+                  <Icon name="md-call" size={24} color="gray" />
+                </View>
+                <Text style={MiumiuTheme.listViewText}>
+                  {mobile}
+                </Text>
+                <Icon
+                  style={MiumiuTheme.listViewForwardIndicator}
+                  name="md-information-circle"
+                  size={22}
+                  color="#4285F4"
+                />
+              </TouchableOpacity>
+              <View style={styles.separatorContainer}>
+                <View style={styles.separator} />
+              </View>
+              <TouchableOpacity style={styles.row} onPress={() => this.emailInfoTapped(email)}>
+                <View style={styles.iconContainer}>
+                  <Icon name="md-mail" size={24} color="gray" />
+                </View>
+                <Text style={MiumiuTheme.listViewText}>
+                  {email}
+                </Text>
+                <Icon
+                  style={MiumiuTheme.listViewForwardIndicator}
+                  name="md-information-circle"
+                  size={22} color="#4285F4"
+                />
+              </TouchableOpacity>
+            </View>
+          }
+          { isRequesting &&
+            <View style={MiumiuTheme.paginationView}>
+              <ActivityIndicator />
+            </View>
+          }
+        </View>
+        <HUD ref={(ref) => { this.HUD = ref; }} type="success" message="已複製到剪貼簿" />
+      </View>
+    );
+  }
+}
+
 const mapStateToProps = (state, ownProps) => {
   const { contactInfo } = state;
   return {
@@ -228,5 +242,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchContactInfo }
+  { fetchContactInfo },
 )(ContactUs);
